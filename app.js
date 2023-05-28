dedd6e189d6b377dcac318c37d65ec134fec6c8e
Tolulope Oke <oketolulope3@gmail.com>
2023-05-28 11:30:49 +0100

    feat: set up environments

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+
+var dotenv = require('dotenv');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+exports.dcy = app;
+// module.exports = app;
+
+//const projectId = process.env.STAGING_PROJECT_ID;
