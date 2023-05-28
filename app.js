b082a268b4cfb3756fa6bf6f8d65032844996f03
Tolulope Oke <oketolulope3@gmail.com>
2023-05-28 13:46:02 +0100

    feat: add config

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var dotenv = require('dotenv');
+
+const { Config } = require('./configs/config');
+
 // var path = require('path');
 // var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,13 +13,16 @@ var usersRouter = require('./routes/users');
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 var app = express();
 
-console.log('listening');
-console.log(process.env.PORT);
-app.listen(process.env.PORT || 9000);
+const port = Number(Config.PORT);
+app.listen(port, () => console.log(`
+  ==================================
+  🚀 Server running on port ${port}!🚀
+  ==================================
+`));
+
 app.use(logger('dev'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// module.exports = app;
-exports.dcy = app;
+module.exports = app;
